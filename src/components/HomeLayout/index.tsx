628bae656cbd36d46acc2624720ad7c7e8086bb0
Rith <harith.trmz@gmail.com>
2023-10-13 03:14:50 +0800

    styling the contact form and add credential component

--- src/components/HomeLayout/index.tsx
+++ src/components/HomeLayout/index.tsx
@@ -6,7 +6,9 @@ import {
   AiFillYoutube,
 } from "react-icons/ai";
 import Image from "next/image";
-import { useState } from "react";
+import { useState, useEffect } from "react";
+import { FieldError, useForm } from "react-hook-form";
+import useWeb3Forms from "@web3forms/react";
 import deved from "../../../public/dev-ed-wave.png";
 import rith from "../../../public/rith.png";
 import code from "../../../public/code.png";
@@ -22,27 +24,37 @@ import web6 from "../../../public/web6.png";
 const HomeLayout = () => {
   const [darkMode, setDarkMode] = useState<boolean>(false);
   const [result, setResult] = useState<string>("");
+  const {
+    register,
+    handleSubmit,
+    reset,
+    formState: { errors, isSubmitSuccessful, isSubmitting },
+  } = useForm({
+    mode: "onTouched",
+  });
+  const [isSuccess, setIsSuccess] = useState<boolean>(false);
+  const [message, setMessage] = useState<string>("");
 
-  const onSubmit = async (event: any) => {
-    event.preventDefault();
-    setResult("Sending....");
-    const formData = new FormData(event.target);
+  // Please update the Access Key in the .env
+  const apiKey =
+    process.env.PUBLIC_ACCESS_KEY || "abc4856b-3af9-4c3f-8f45-17e199dc152b";
 
-    formData.append("access_key", "abc4856b-3af9-4c3f-8f45-17e199dc152b");
-
-    const res = await fetch("https://api.web3forms.com/submit", {
-      method: "POST",
-      body: formData,
-    }).then((res) => res.json());
-
-    if (res.success) {
-      console.log("Success", res);
-      setResult(res.message);
-    } else {
-      console.log("Error", res);
-      setResult(res.message);
-    }
-  };
+  const { submit: onSubmit } = useWeb3Forms({
+    access_key: apiKey,
+    settings: {
+      from_name: "Rith Inc",
+      subject: "Enquiry from your website",
+    },
+    onSuccess: (msg, data) => {
+      setIsSuccess(true);
+      setMessage(msg);
+      reset();
+    },
+    onError: (msg, data) => {
+      setIsSuccess(false);
+      setMessage(msg);
+    },
+  });
 
   return (
     <div className={darkMode ? "dark" : ""}>
@@ -60,9 +72,9 @@ const HomeLayout = () => {
               <li>
                 <a
                   className="bg-gradient-to-r from-cyan-500 to-teal-500 text-white px-4 py-2 rounded-md ml-8"
-                  href="#"
+                  href="/credentials"
                 >
-                  Resume
+                  Credentials
                 </a>
               </li>
             </ul>
@@ -188,14 +200,127 @@ const HomeLayout = () => {
         </section>
 
         <section>
-          <h1>React File Upload Form</h1>
-          <form onSubmit={onSubmit}>
-            <input type="text" name="name" />
-            <input type="email" name="email" />
-            <textarea name="message"></textarea>
-            <input type="submit" />
+          <h3 className="text-3xl py-1 dark:text-white ">Contact</h3>
+          <form onSubmit={handleSubmit(onSubmit)} className="mt-5 pb-10">
+            <input
+              type="checkbox"
+              id=""
+              className="hidden"
+              style={{ display: "none" }}
+              {...register("botcheck")}
+            ></input>
+
+            <div className="mb-5">
+              <input
+                type="text"
+                placeholder="Full Name"
+                autoComplete="false"
+                className={`w-full px-4 py-3 border-2 placeholder:text-gray-800 dark:text-white rounded-md outline-none dark:placeholder:text-gray-200 dark:bg-gray-900   focus:ring-4  ${
+                  errors.name
+                    ? "border-red-600 focus:border-red-600 ring-red-100 dark:ring-0"
+                    : "border-gray-300 focus:border-gray-600 ring-gray-100 dark:border-gray-600 dark:focus:border-white dark:ring-0"
+                }`}
+                {...register("name", {
+                  required: "Full name is required",
+                  maxLength: 80,
+                })}
+              />
+              {errors.name && (
+                <div className="mt-1 text-red-600">
+                  <small>{(errors.name as FieldError).message}</small>
+                </div>
+              )}
+            </div>
+
+            <div className="mb-5">
+              <label htmlFor="email_address" className="sr-only">
+                Email Address
+              </label>
+              <input
+                id="email_address"
+                type="email"
+                placeholder="Email Address"
+                autoComplete="false"
+                className={`w-full px-4 py-3 border-2 placeholder:text-gray-800 dark:text-white rounded-md outline-none dark:placeholder:text-gray-200 dark:bg-gray-900   focus:ring-4  ${
+                  errors.email
+                    ? "border-red-600 focus:border-red-600 ring-red-100 dark:ring-0"
+                    : "border-gray-300 focus:border-gray-600 ring-gray-100 dark:border-gray-600 dark:focus:border-white dark:ring-0"
+                }`}
+                {...register("email", {
+                  required: "Enter your email",
+                  pattern: {
+                    value: /^\S+@\S+$/i,
+                    message: "Please enter a valid email",
+                  },
+                })}
+              />
+              {errors.email && (
+                <div className="mt-1 text-red-600">
+                  <small>{(errors.email as FieldError).message}</small>
+                </div>
+              )}
+            </div>
+
+            <div className="mb-3">
+              <textarea
+                placeholder="Your Message"
+                className={`w-full px-4 py-3 border-2 placeholder:text-gray-800 dark:text-white dark:placeholder:text-gray-200 dark:bg-gray-900   rounded-md outline-none  h-36 focus:ring-4  ${
+                  errors.message
+                    ? "border-red-600 focus:border-red-600 ring-red-100 dark:ring-0"
+                    : "border-gray-300 focus:border-gray-600 ring-gray-100 dark:border-gray-600 dark:focus:border-white dark:ring-0"
+                }`}
+                {...register("message", {
+                  required: "Enter your Message",
+                })}
+              />
+              {errors.message && (
+                <div className="mt-1 text-red-600">
+                  <small>{(errors.message as FieldError).message}</small>
+                </div>
+              )}
+            </div>
+
+            <button
+              type="submit"
+              className="w-full py-4 font-semibold text-white transition-colors bg-gray-900 rounded-md hover:bg-gray-800 focus:outline-none focus:ring-offset-2 focus:ring focus:ring-gray-200 px-7 dark:bg-white dark:text-black "
+            >
+              {isSubmitting ? (
+                <svg
+                  className="w-5 h-5 mx-auto text-white dark:text-black animate-spin"
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                >
+                  <circle
+                    className="opacity-25"
+                    cx="12"
+                    cy="12"
+                    r="10"
+                    stroke="currentColor"
+                    strokeWidth="4"
+                  ></circle>
+                  <path
+                    className="opacity-75"
+                    fill="currentColor"
+                    d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"
+                  ></path>
+                </svg>
+              ) : (
+                "Send Message"
+              )}
+            </button>
           </form>
-          <span>{result}</span>
+
+          {isSubmitSuccessful && isSuccess && (
+            <div className="pb-5 text-sm text-center text-green-50">
+              {message || "Success. Message sent successfully"}
+            </div>
+          )}
+          {isSubmitSuccessful && !isSuccess && (
+            <div className="pb-5 text-sm text-center text-red-500">
+              {message || "Something went wrong. Please try later."}
+            </div>
+          )}
         </section>
       </main>
     </div>
