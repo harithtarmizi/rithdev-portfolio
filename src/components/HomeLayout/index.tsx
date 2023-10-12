2aadae44d10549895d18400030a14572e8aad912
Rith <harith.trmz@gmail.com>
2023-10-12 22:59:40 +0800

    restructure folder

---
+++ src/components/HomeLayout/index.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+const HomeLayout = () => {
+  return (
+    <main>
+      <section className="bg-blue-700 min-h-screen"> 
+        <nav>
+          <h1 className="font-burtons text-xl">rithdev</h1>
+          <h1 className="text-3xl font-bold underline">Hello world!</h1>
+          <ul></ul>
+        </nav>
+      </section>
+    </main>
+  );
+};
+
+export default HomeLayout;
